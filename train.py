6e4daa5e9c93cd15dda7e7bd0cf5ccea9e989e92
pontusnorman123 <94538098+pontusnorman123@users.noreply.github.com>
2023-04-30 16:01:25 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,35 +1,40 @@
-# Wine Quality Sample
-def train(in_alpha, in_l1_ratio):
-    import os
-    import warnings
-    import sys
+# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
+# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
 
-    import pandas as pd
-    import numpy as np
-    from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-    from sklearn.model_selection import train_test_split
-    from sklearn.linear_model import ElasticNet
+import os
+import warnings
+import sys
 
-    import mlflow
-    import mlflow.sklearn
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 
-    import logging
+import logging
 
-    logging.basicConfig(level=logging.WARN)
-    logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
-    def eval_metrics(actual, pred):
-        rmse = np.sqrt(mean_squared_error(actual, pred))
-        mae = mean_absolute_error(actual, pred)
-        r2 = r2_score(actual, pred)
-        return rmse, mae, r2
 
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
     # Read the wine-quality csv file from the URL
     csv_url = (
-        "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
     )
     try:
         data = pd.read_csv(csv_url, sep=";")
@@ -47,42 +52,36 @@ def train(in_alpha, in_l1_ratio):
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    # Set default values if no alpha is provided
-    if float(in_alpha) is None:
-        alpha = 0.5
-    else:
-        alpha = float(in_alpha)
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    # Set default values if no l1_ratio is provided
-    if float(in_l1_ratio) is None:
-        l1_ratio = 0.5
-    else:
-        l1_ratio = float(in_l1_ratio)
-      
-    mlflow.sklearn.autolog()
-
-
-    # Useful for multiple runs (only doing one run in this sample notebook)
     with mlflow.start_run():
-        # Execute ElasticNet
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
-        # Evaluate Metrics
         predicted_qualities = lr.predict(test_x)
+
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        # Print out metrics
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
-        # Log parameter, metrics, and model to MLflow
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        mlflow.sklearn.log_model(lr, "model")
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+        else:
+            mlflow.sklearn.log_model(lr, "model")
