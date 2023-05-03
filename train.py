d7982a3d8dc645b771f3f46eb8cda37e91cdc2ef
pontusnorman123 <94538098+pontusnorman123@users.noreply.github.com>
2023-05-03 14:27:55 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,9 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
-
 import os
-import warnings
+#import warnings
 import sys
 
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from urllib.parse import urlparse
@@ -21,33 +16,27 @@ logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
-
-
 if __name__ == "__main__":
-    warnings.filterwarnings("ignore")
+    
+    #warnings.filterwarnings("ignore")
     np.random.seed(40)
 
-    csv_url = (
+    url = (
         "https://raw.githubusercontent.com/pontusnorman123/devops_tutorial/main/tutorialQuality.csv"
     )
     data = pd.read_csv(csv_url, sep=";")
 
-    # Split the data into training and test sets. (0.75, 0.25) split.
+    # Split the data into a training set and test set
     train, test = train_test_split(data)
 
-    # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
     test_x = test.drop(["quality"], axis=1)
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    // Read model training parameters
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.3
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.3
 
     mlflow.sklearn.autolog()
     
